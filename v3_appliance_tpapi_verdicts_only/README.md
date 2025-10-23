d0703174d856a8a1493f5d8137a8b2e89f3fb305
Andy Nicholson <andyn@checkpoint.com>
2025-10-23 15:48:23 +0100

    v3 -

---
+++ v3_appliance_tpapi_verdicts_only/README.md
@@ -0,0 +1,46 @@
+# te_api
+A Python client side utility for using Threat Emulation API calls to an appliance.
+
+It includes Upload, Query and Download API calls, using the Cloud API format ( …/tecloud/api/v#/file/..).
+
+### The flow
+Going through the input directory and handling each file in order to get its Threat Emulation results.
+
+For each file:
+
+      1. Querying te cache for already existing results by the file sha1.
+
+           If results exist then goto #4, otherwise- continue to #2
+    
+      2. Uploading the file to the appliance for te and te_eb features.
+    
+      3. If upload result is upload_success then querying te and te_eb until receiving te results.
+
+           (Note, te_eb results of early malicious verdict might be received earlier during the queries in between)
+    
+      4. Writing to output file - the last query/upload response info.
+    
+      5. If te result is found then displaying the verdict.  If verdict is malicious then also downloading the TE report.
+
+### Usage
+~~~~
+python te_api.py --help
+
+usage: te_api.py [-h] [-id INPUT_DIRECTORY] [-od OUTPUT_DIRECTORY]
+                 [-ip APPLIANCE_IP]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -id INPUT_DIRECTORY, --input_directory INPUT_DIRECTORY
+                        the input files folder to be scanned by TE
+  -od OUTPUT_DIRECTORY, --output_directory OUTPUT_DIRECTORY
+                        the output folder with TE results
+  -ip APPLIANCE_IP, --appliance_ip APPLIANCE_IP
+                        the appliance ip address
+
+~~~~
+It is also possible to change the optional arguments default values within te_api.py
+
+### References
+* Additional Threat Emulation API info: [sk167161](https://supportcenter.checkpoint.com/supportcenter/portal?eventSubmit_doGoviewsolutiondetails=&solutionid=sk167161)
+* te_eb feature: [sk117168 chapter 4](https://supportcenter.checkpoint.com/supportcenter/portal?eventSubmit_doGoviewsolutiondetails=&solutionid=sk117168#New%20Public%20API%20Interface)
---
+++ v3_appliance_tpapi_verdicts_only/te_api.py
@@ -0,0 +1,91 @@
+"""
+te_api v3.0
+A Python client-side utility for interacting with the Threat Emulation API.
+Features:
+  - Scan input files in a specified directory
+  - Handle TE and TE_EB processing via the appliance
+  - Store results in an output directory
+  - Support concurrent processing of multiple files. Requires a command line argument.
+"""
+
+from te_file_handler import TE
+import os
+import argparse
+import concurrent.futures
+
+# Following variables can be assigned and used instead of adding them as arguments when running the te_api.py .
+#  input_directory and reports_directory have the following default settings.
+#  Using the following input directory default setting means - assuming that the input files to handle are in
+#   already existing folder :  ..appliance_tpapi/te_api/input_files
+#  Using the following reports_directory default setting means - creating/using the output directory :
+#   ..appliance_tpapi/te_api/te_response_data
+input_directory = "input_files"
+reports_directory = "te_response_data"
+appliance_ip = ""
+
+
+def main():
+    """
+    1. Get the optional arguments (if any): the input-directory, the output-directory and appliance-ip.
+    2. Accordingly set the api-url, and create the output directory.
+    3. Go though all input files in the input directory.
+        Handling each input file is described in TE class in te_file_handler.py:
+    """
+    global input_directory
+    global reports_directory
+    global appliance_ip
+    global concurrency
+    global url
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-in", "--input_directory", help="the input files folder to be scanned by TE")
+    parser.add_argument("-rep", "--reports_directory", help="the output folder with TE results")
+    parser.add_argument("-ip", "--appliance_ip", help="the appliance ip address")
+    parser.add_argument('-n', '--concurrency', type=int, help='Number of concurrent loops')
+    args = parser.parse_args()
+    if args.input_directory:
+        input_directory = args.input_directory
+    print("The input files directory to be scanned by TE : {}".format(input_directory))
+    if not os.path.exists(input_directory):
+        print("\n\n  --> The input files directory {} does not exist !\n\n".format(input_directory))
+        parser.print_help()
+        return
+    if args.reports_directory:
+        reports_directory = args.reports_directory
+    print("The output directory with TE results : {}".format(reports_directory))
+    if not os.path.exists(reports_directory):
+        print("Pre-processing: creating te_api output directory {}".format(reports_directory))
+        try:
+            os.mkdir(reports_directory)
+        except Exception as E1:
+            print("could not create te_api output directory, because: {}".format(E1))
+            return
+    if args.appliance_ip:
+        appliance_ip = args.appliance_ip
+    if not appliance_ip:
+        print("\n\n  --> Missing appliance_ip !\n\n")
+        parser.print_help()
+        return
+    print("The appliance ip address : {}".format(appliance_ip))
+    url = "https://" + appliance_ip + ":18194/tecloud/api/v1/file/"
+
+
+    # A loop over the files in the input folder
+    files = os.listdir(input_directory)
+    print("Begin handling input files by TE")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=args.concurrency) as executor:
+        executor.map(process_files, files)
+
+def process_files(file_name):
+    try:
+        full_path = os.path.join(input_directory, file_name)
+        print("Handling file: {} by TE".format(file_name))
+        te = TE(url, file_name, full_path, reports_directory)
+        te.handle_file()
+    except Exception as E:
+        print("Could not handle file: {} because: {}. Continue to handle the next file.".format(file_name, E))
+
+
+if __name__ == '__main__':
+    main()
+
